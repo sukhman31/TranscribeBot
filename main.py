9a0cf71b605da4ea5eded8631c89116fadd3624a
Sukhman Singh <ssingh8_be20@thapar.edu>
2023-05-04 14:16:49 +0530

    function app

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import streamlit as st
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import TextLoader
+from langchain.vectorstores import Chroma
+from langchain import PromptTemplate, LLMChain, OpenAI
+
+
+loader = TextLoader('video_transcription/final_file')
+docs = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=0)
+docs = text_splitter.split_documents(docs)
+db = Chroma.from_documents(docs)
 
 st.set_page_config(page_title="Andrej Karpathy Chat Bot", page_icon=":robot:")
 st.header('Andrej Karpathy Bot')
@@ -14,7 +25,7 @@ with col1:
                 the complexities of deep learning and artificial intelligence. \
                 Our chatbot attempts to answer any queries you might have regarding \
                 this [playlist](https://www.youtube.com/playlist?list=PLAqhIrjkxbuWI23v9cThsA9GvCAUhRvKZ).")
-
+    
 
 st.markdown("## Enter Your Query")
 
@@ -25,4 +36,14 @@ def get_text():
 query = get_text()
 
 if query:
-    st.write(query)
+    ans = db.similarity_search(query,k=1)
+    ans = ans[0].page_content
+    llm = OpenAI(temperature=0)
+    template="""Please use the following context to answer questions.
+    Context: {ans}
+    Question: {query}
+    Answer:"""
+
+    prompt = PromptTemplate(template=template, input_variables=["ans","query"])
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    st.write(llm_chain.predict(ans = ans, query = query))
